88376dd785e47da30794b17e64149d03657edc56
Jon-Nutman <jonnut730@gmail.com>
2021-01-27 00:32:26 -0500

    partial

--- main.js
+++ main.js
@@ -1,27 +1,63 @@
+// ADVENTURE MODE
 
 
+// make buttons for numbers 0-9 
+// make buttons for operators (+ - * /)
 
-document.querySelector('#sub').addEventListener('submit', function (e) {
+// on key click add value of button to string
+
+const keys = document.querySelector('.keys')
+
+const operators = document.querySelector('.operator')
+
+let num1 = ''
+let num2 = ''
+
+keys.addEventListener('click', function(e) {
     e.preventDefault()
-    // console.log(e.submitter.id)
-    let answer = ''
-    let num1 = document.getElementById("num1").value
-    let num2 = document.getElementById("num2").value
-    let operator = e.submitter.id
+let btn = e.target.value
+    // console.log(e.target.value)
+// let nums = ('0','1', '2','3','4','5','6','7','8','9')
 
-    // console.log(operator)
-    
-    if (operator == "add"){
-        answer = Number(num1) + Number(num2)
-    }else if(operator == "subtract"){
-        answer = Number(num1) - Number(num2)
-    }else if(operator == "times"){
-        answer = (Number(num1)) * (Number(num2))
-    }else if(operator == "divide"){
-        answer = (Number(num1)) / (Number(num2))
+let nums = 
+
+    if (btn.includes == nums{
+        console.log(btn)
+    //   return num1 = btn
+     
     }
-    return(document.getElementById("answer").value = answer)
+
 })
+
+
+    
+   
+
+
+
+// // EXPLORE MODE
+
+// document.querySelector('#sub').addEventListener('submit', function (e) {
+//     e.preventDefault()
+//     // console.log(e.submitter.id)
+//     let answer = ''
+//     let num1 = document.getElementById("num1").value
+//     let num2 = document.getElementById("num2").value
+//     let operator = e.submitter.id
+
+//     // console.log(operator)
+    
+//     if (operator == "add"){
+//         answer = Number(num1) + Number(num2)
+//     }else if(operator == "subtract"){
+//         answer = Number(num1) - Number(num2)
+//     }else if(operator == "times"){
+//         answer = (Number(num1)) * (Number(num2))
+//     }else if(operator == "divide"){
+//         answer = (Number(num1)) / (Number(num2))
+//     }
+//     return(document.getElementById("answer").value = answer)
+// })
 
 // console.log(output)
 
